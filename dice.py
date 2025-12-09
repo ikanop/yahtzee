7f6192b99ea696b121a602e37ec45e4b2e822d3d
ikanop <113450482+ikanop@users.noreply.github.com>
2025-12-09 13:49:10 +0100

    made the game yahtzee

--- dice.py
+++ dice.py
@@ -1,15 +1,23 @@
 import random
 
-def roll_function():
-    return random.randint(1,6)
+def roll_function(amount):
+    rolls = []
+    for _ in range(amount):
+        rolls.append(random.randint(1, 6))
+    return rolls
 
-rolls = [roll_function() for _ in range(3)]
+rolls = roll_function(5)
+pairs = [x for x in set(rolls) if rolls.count(x) == 2]
 
 print("You rolled:", *rolls)
 
-if rolls.count(6) == 3:
-    print("All three rolls are sixes! 🎉")
-elif len(set(rolls)) < len(rolls):
-    print("At least one number is repeated!")
-else:
-    print("All rolls are different!")
+if any(rolls.count(x) == len(rolls) for x in set(rolls)):
+    print("YAHTZEE! 🎉")
+elif any(rolls.count(x) == 4 for x in set(rolls)):
+    print("You got four of a kind!")
+elif any(rolls.count(x) == 3 for x in set(rolls)):
+    print("You got three of a kind!")
+elif len(pairs) == 2:
+    print("You got two pairs!")
+elif pairs:
+    print("You got a pair!")
